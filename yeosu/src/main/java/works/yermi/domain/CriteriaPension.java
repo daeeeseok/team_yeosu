bdb5a8d688850db552e5d6d19619b94729ca9b65
ds <ds@DESKTOP-A36LEB9>
2022-05-30 17:12:51 +0900

    team yeosu

--- yeosu/src/main/java/works/yermi/domain/CriteriaPension.java
+++
@@ -1,9 +0,0 @@
-package works.yermi.domain;
-
-import lombok.Data;
-
-@Data
-public class CriteriaPension {
-	private int amount = 10;
-	private Long lastPensionid = 0L;
-}
--- yeosu/src/main/java/works/yermi/mapper/FavoritesMapper.java
+++
@@ -1,9 +0,0 @@
-package works.yermi.mapper;
-
-import java.util.List;
-
-import works.yermi.domain.FavoritesVO;
-
-public interface FavoritesMapper {
-	public List<FavoritesVO> getList();
-}
